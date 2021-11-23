8a4376ab0856567c692c4b198edca331417e73f8
ratataca <gywls18@koreatech.ac.kr>
2021-11-23 12:21:59 +0900

    초기 커밋.

---
+++ myCalc/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace myCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num_1 = new System.Windows.Forms.Button();
+            this.num_2 = new System.Windows.Forms.Button();
+            this.num_3 = new System.Windows.Forms.Button();
+            this.num_6 = new System.Windows.Forms.Button();
+            this.num_5 = new System.Windows.Forms.Button();
+            this.num_4 = new System.Windows.Forms.Button();
+            this.num_9 = new System.Windows.Forms.Button();
+            this.num_8 = new System.Windows.Forms.Button();
+            this.num_7 = new System.Windows.Forms.Button();
+            this.num_0 = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.subtract = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.division = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.text_result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // num_1
+            // 
+            this.num_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num_1.Location = new System.Drawing.Point(19, 195);
+            this.num_1.Name = "num_1";
+            this.num_1.Size = new System.Drawing.Size(138, 50);
+            this.num_1.TabIndex = 0;
+            this.num_1.Text = "1";
+            this.num_1.UseVisualStyleBackColor = true;
+            // 
+            // num_2
+            // 
+            this.num_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num_2.Location = new System.Drawing.Point(163, 195);
+            this.num_2.Name = "num_2";
+            this.num_2.Size = new System.Drawing.Size(138, 50);
+            this.num_2.TabIndex = 1;
+            this.num_2.Text = "2";
+            this.num_2.UseVisualStyleBackColor = true;
+            // 
+            // num_3
+            // 
+            this.num_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num_3.Location = new System.Drawing.Point(307, 195);
+            this.num_3.Name = "num_3";
+            this.num_3.Size = new System.Drawing.Size(138, 50);
+            this.num_3.TabIndex = 2;
+            this.num_3.Text = "3";
+            this.num_3.UseVisualStyleBackColor = true;
+            // 
+            // num_6
+            // 
+            this.num_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num_6.Location = new System.Drawing.Point(307, 139);
+            this.num_6.Name = "num_6";
+            this.num_6.Size = new System.Drawing.Size(138, 50);
+            this.num_6.TabIndex = 5;
+            this.num_6.Text = "6";
+            this.num_6.UseVisualStyleBackColor = true;
+            // 
+            // num_5
+            // 
+            this.num_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num_5.Location = new System.Drawing.Point(163, 139);
+            this.num_5.Name = "num_5";
+            this.num_5.Size = new System.Drawing.Size(138, 50);
+            this.num_5.TabIndex = 4;
+            this.num_5.Text = "5";
+            this.num_5.UseVisualStyleBackColor = true;
+            // 
+            // num_4
+            // 
+            this.num_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num_4.Location = new System.Drawing.Point(19, 139);
+            this.num_4.Name = "num_4";
+            this.num_4.Size = new System.Drawing.Size(138, 50);
+            this.num_4.TabIndex = 3;
+            this.num_4.Text = "4";
+            this.num_4.UseVisualStyleBackColor = true;
+            // 
+            // num_9
+            // 
+            this.num_9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num_9.Location = new System.Drawing.Point(307, 83);
+            this.num_9.Name = "num_9";
+            this.num_9.Size = new System.Drawing.Size(138, 50);
+            this.num_9.TabIndex = 8;
+            this.num_9.Text = "9";
+            this.num_9.UseVisualStyleBackColor = true;
+            // 
+            // num_8
+            // 
+            this.num_8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num_8.Location = new System.Drawing.Point(163, 83);
+            this.num_8.Name = "num_8";
+            this.num_8.Size = new System.Drawing.Size(138, 50);
+            this.num_8.TabIndex = 7;
+            this.num_8.Text = "8";
+            this.num_8.UseVisualStyleBackColor = true;
+            // 
+            // num_7
+            // 
+            this.num_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num_7.Location = new System.Drawing.Point(19, 83);
+            this.num_7.Name = "num_7";
+            this.num_7.Size = new System.Drawing.Size(138, 50);
+            this.num_7.TabIndex = 6;
+            this.num_7.Text = "7";
+            this.num_7.UseVisualStyleBackColor = true;
+            // 
+            // num_0
+            // 
+            this.num_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num_0.Location = new System.Drawing.Point(163, 251);
+            this.num_0.Name = "num_0";
+            this.num_0.Size = new System.Drawing.Size(138, 50);
+            this.num_0.TabIndex = 9;
+            this.num_0.Text = "0";
+            this.num_0.UseVisualStyleBackColor = true;
+            this.num_0.Click += new System.EventHandler(this.num_0_Click);
+            // 
+            // equal
+            // 
+            this.equal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.equal.Location = new System.Drawing.Point(307, 251);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(138, 50);
+            this.equal.TabIndex = 10;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            this.equal.Click += new System.EventHandler(this.equal_Click);
+            // 
+            // add
+            // 
+            this.add.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.add.Location = new System.Drawing.Point(451, 83);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(138, 50);
+            this.add.TabIndex = 11;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // subtract
+            // 
+            this.subtract.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.subtract.Location = new System.Drawing.Point(451, 139);
+            this.subtract.Name = "subtract";
+            this.subtract.Size = new System.Drawing.Size(138, 50);
+            this.subtract.TabIndex = 12;
+            this.subtract.Text = "-";
+            this.subtract.UseVisualStyleBackColor = true;
+            this.subtract.Click += new System.EventHandler(this.subtract_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.multiply.Location = new System.Drawing.Point(451, 195);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(138, 50);
+            this.multiply.TabIndex = 13;
+            this.multiply.Text = "X";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // division
+            // 
+            this.division.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.division.Location = new System.Drawing.Point(451, 251);
+            this.division.Name = "division";
+            this.division.Size = new System.Drawing.Size(138, 50);
+            this.division.TabIndex = 14;
+            this.division.Text = "/";
+            this.division.UseVisualStyleBackColor = true;
+            this.division.Click += new System.EventHandler(this.division_Click);
+            // 
+            // button15
+            // 
+            this.button15.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button15.Location = new System.Drawing.Point(451, 32);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(138, 45);
+            this.button15.TabIndex = 15;
+            this.button15.Text = "Clear";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // text_result
+            // 
+            this.text_result.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.text_result.Location = new System.Drawing.Point(19, 32);
+            this.text_result.Name = "text_result";
+            this.text_result.Size = new System.Drawing.Size(426, 43);
+            this.text_result.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 316);
+            this.Controls.Add(this.text_result);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.division);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.subtract);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.num_0);
+            this.Controls.Add(this.num_9);
+            this.Controls.Add(this.num_8);
+            this.Controls.Add(this.num_7);
+            this.Controls.Add(this.num_6);
+            this.Controls.Add(this.num_5);
+            this.Controls.Add(this.num_4);
+            this.Controls.Add(this.num_3);
+            this.Controls.Add(this.num_2);
+            this.Controls.Add(this.num_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button num_1;
+        private System.Windows.Forms.Button num_2;
+        private System.Windows.Forms.Button num_3;
+        private System.Windows.Forms.Button num_6;
+        private System.Windows.Forms.Button num_5;
+        private System.Windows.Forms.Button num_4;
+        private System.Windows.Forms.Button num_9;
+        private System.Windows.Forms.Button num_8;
+        private System.Windows.Forms.Button num_7;
+        private System.Windows.Forms.Button num_0;
+        private System.Windows.Forms.Button equal;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button subtract;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button division;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.TextBox text_result;
+    }
+}
+
---
+++ myCalc/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+static class Constants
+{
+    public const int ADD = 1;
+    public const int SUBTRACT = 2; 
+    public const int MULTIPLY = 3; 
+    public const int DIVISION = 4; 
+}
+
+
+namespace myCalc
+{
+    public partial class Form1 : Form
+    {
+        double input_num_1 = 0;
+        double input_num_2 = 0;
+        bool isError = false;
+        int type = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            num_1.Click += num_0_Click;
+            num_2.Click += num_0_Click;
+            num_3.Click += num_0_Click;
+            num_4.Click += num_0_Click;
+            num_5.Click += num_0_Click;
+            num_6.Click += num_0_Click;
+            num_7.Click += num_0_Click;
+            num_8.Click += num_0_Click;
+            num_9.Click += num_0_Click;
+            
+        }
+        
+        // 숫자 버튼에 대한 이벤트
+        private void num_0_Click(object sender, EventArgs e)
+        {
+            Button self = (Button)sender;
+            if (isError == true)
+            {
+                text_result.Text = "";
+                isError = false;
+            }
+            text_result.Text += self.Text;
+            
+        }
+
+        // Clear 버튼에 대한 이벤트
+        private void button15_Click(object sender, EventArgs e)
+        {
+            text_result.Text = "";
+            type = 0;
+
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            if (type == 0)
+            {
+                type = Constants.ADD;
+                text_result.Text += "+";
+            }
+            else {
+                text_result.Text = "수식이 잘못되었습니다.";
+                isError = true;
+            }
+        }
+
+        private void subtract_Click(object sender, EventArgs e)
+        {
+            if (type == 0)
+            {
+                type = Constants.SUBTRACT;
+                text_result.Text += "-";
+            }
+            else
+            {
+                text_result.Text = "수식이 잘못되었습니다.";
+                isError = true;
+            }
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            
+            if (type == 0)
+            {
+                type = Constants.MULTIPLY;
+                text_result.Text += "*";
+            }
+            else
+            {
+                text_result.Text = "수식이 잘못되었습니다.";
+                isError = true;
+            }
+        }
+
+        private void division_Click(object sender, EventArgs e)
+        {
+            if (type == 0)
+            {
+                type = Constants.DIVISION;
+                text_result.Text += "/";
+            }
+            else
+            {
+                text_result.Text = "수식이 잘못되었습니다.";
+                isError = true;
+            }
+        }
+      
+        private void equal_Click(object sender, EventArgs e)
+        {
+            double result = 0.0;
+
+            if (text_result.Text == "수식이 잘못되었습니다.") {
+                isError = true;
+                type = 0;
+                return;
+            }
+
+            switch (type) {
+                case Constants.ADD:
+                    String[] subsAdd = text_result.Text.Split("+");
+
+                    if (subsAdd[1] != "" )
+                    {
+                        input_num_1 = double.Parse(subsAdd[0]);
+                        input_num_2 = double.Parse(subsAdd[1]);
+
+                        result = input_num_1 + input_num_2;
+                        text_result.Text = result.ToString();
+                    }
+                    else {
+                        text_result.Text = "수식이 잘못되었습니다.";
+                        isError = true;
+                        type = 0;
+                        return;
+                    }                    
+                    break;
+
+                case Constants.SUBTRACT:
+                    String[] subsSub = text_result.Text.Split("-");
+
+                    if (subsSub[1] != "")
+                    {
+                        input_num_1 = double.Parse(subsSub[0]);
+                        input_num_2 = double.Parse(subsSub[1]);
+
+                        result = input_num_1 - input_num_2;
+                        text_result.Text = result.ToString();
+                    }
+                    else
+                    {
+                        text_result.Text = "수식이 잘못되었습니다.";
+                        isError = true;
+                        type = 0;
+                        return;
+                    }                   
+                    break;
+
+                case Constants.MULTIPLY:
+                    String[] subsMul = text_result.Text.Split("*");
+
+                    if (subsMul[1] != "")
+                    {
+                        input_num_1 = double.Parse(subsMul[0]);
+                        input_num_2 = double.Parse(subsMul[1]);
+
+                        result = input_num_1 * input_num_2;
+                        text_result.Text = result.ToString();
+                    }
+                    else
+                    {
+                        text_result.Text = "수식이 잘못되었습니다.";
+                        isError = true;
+                        type = 0;
+                        return;
+                    }
+                    break;
+
+                case Constants.DIVISION:
+                    String[] subsDiv = text_result.Text.Split("/");
+
+                    if (subsDiv[1] != "")
+                    {
+                        input_num_1 = double.Parse(subsDiv[0]);
+                        input_num_2 = double.Parse(subsDiv[1]);
+
+                        result = input_num_1 / input_num_2;
+                        text_result.Text = result.ToString();
+                    }
+                    else
+                    {
+                        text_result.Text = "수식이 잘못되었습니다.";
+                        isError = true;
+                        type = 0;
+                        return;
+                    }
+                    break;
+
+                default:
+                    text_result.Text = "선택된 연산자가 없습니다.";
+                    isError = true;
+                    type = 0;
+                    break;
+            }
+            
+            type = 0;
+        }       
+    }
+}
